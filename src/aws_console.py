bd37a86638840f946857d39abd5ff62ac49c74af
Jatin Jindal <jatinjindal.2003@gmail.com>
2022-09-21 19:46:48 +0530

    frustration feature: Add a console because AWS

---
+++ src/aws_console.py
@@ -0,0 +1,27 @@
+import pymysql
+from dotenv import load_dotenv
+import os
+load_dotenv(os.path.join(os.path.dirname(__file__), '..', '.env'))
+
+
+host = os.getenv('AWS_HOSTNAME')
+user = os.getenv('AWS_USERNAME')
+password = os.getenv('AWS_PASSWORD')
+database = os.getenv('AWS_DATABASE')
+
+connection = pymysql.connect(
+    host=host, user=user, passwd=password, database=database)
+os.system('cls|clear')
+with connection:
+    cur = connection.cursor()
+    while True:
+        query = input(">> ")
+        if query in ("?", "help", "HELP", "h", "H", "-h", "--help"):
+            print(">> Type 'EXIT' to exit the console")
+        if query == "EXIT":
+            break
+        try:
+            cur.execute(query)
+            print(cur.fetchall())
+        except:
+            print("Invalid Query")
